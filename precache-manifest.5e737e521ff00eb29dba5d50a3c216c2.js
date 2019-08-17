db496c2f052333d394fe3fb6b6fb78a515d729db
Aman Jagga <aman.jagga@practo.com>
2019-08-18 02:35:49 +0530

    Initial dist subtree commit

---
+++ precache-manifest.5e737e521ff00eb29dba5d50a3c216c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9a3760160121bb1b9f133152e34c3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d65b2d5509f7b55e86ee",
+    "url": "/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "b7a9f305d99b888f960a",
+    "url": "/static/css/main.ecb84f0f.chunk.css"
+  },
+  {
+    "revision": "d65b2d5509f7b55e86ee",
+    "url": "/static/js/2.30c21baa.chunk.js"
+  },
+  {
+    "revision": "b7a9f305d99b888f960a",
+    "url": "/static/js/main.e27eacf7.chunk.js"
+  },
+  {
+    "revision": "8397f59ae3baca87b8bd",
+    "url": "/static/js/runtime~main.31951d7c.js"
+  }
+]);
